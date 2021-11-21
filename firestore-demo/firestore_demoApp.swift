4394e37549f0d1d47f430c7cc0e60ac8fbbc0da5
Phineas1500 <sriram.kiron@gmail.com>
2021-11-21 12:36:28 -0500

    Initial Commit

---
+++ firestore-demo/firestore_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  firestore_demoApp.swift
+//  firestore-demo
+//
+//  Created by Sriram Kiron on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct firestore_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
