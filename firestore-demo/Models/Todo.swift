f42bad78c80ecfac488b49414a2c461791513427
Phineas1500 <41450967+Phineas1500@users.noreply.github.com>
2021-12-11 23:09:43 -0500

    Add files via upload

---
+++ firestore-demo/Models/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  firestore-demo
+//
+//  Created by Sriram Kiron on 11/22/21.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id: String
+    var name: String
+    var notes: String
+}
